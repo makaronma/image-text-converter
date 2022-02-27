180c70f191e880888e95ff1f27bfba4140cbc11c
makaronma <mckahung250@gmail.com>
2022-02-27 14:59:43 +0800

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { createWorker } from "tesseract.js";
 
 function App() {
+  const [ocr, setOcr] = useState("Recognizing...");
+  const [file, setFile] = useState(null);
+
+  const doOCR = useCallback(async (file) => {
+    const worker = createWorker({
+      logger: (m) => console.log(m),
+    });
+
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text },
+    } = await worker.recognize(
+      "https://tesseract.projectnaptha.com/img/eng_bw.png"
+    );
+    setOcr(text);
+  }, []);
+
+  useEffect(() => {
+    if (!file) return;
+    console.log(file);
+    setOcr("Recognizing...");
+    doOCR(file);
+  }, [doOCR, file]);
+
+  const handleFileSubmit = (e) => {
+    e.preventDefault();
+    setFile(e.target[0].files[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFileSubmit}>
+        <input type="file" name="file" id="file" />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{ocr}</p>
     </div>
   );
 }
